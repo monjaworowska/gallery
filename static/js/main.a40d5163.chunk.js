accdf87c1d4eb9767f5c296c5620d9ce0093e4ad
Monika Jaworowska <mon.jaworowska@gmail.com>
2021-03-17 12:04:39 +0100

    Updates

---
+++ static/js/main.a40d5163.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgallery=this.webpackJsonpgallery||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);n(1);var r=n(3),c=n.n(r),s=n(0),i=function(){return Object(s.jsx)("div",{children:"App"})};c.a.render(Object(s.jsx)(i,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.a40d5163.chunk.js.map
